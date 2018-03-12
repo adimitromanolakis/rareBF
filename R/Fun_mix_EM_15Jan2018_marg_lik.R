2eb8accb4d1973faee16bbfd7bd9bbc2027c2b4a
Apostolos Dimitromanolakis <apostolis@apo>
2018-03-12 12:26:52 -0400

    small changes

--- R/Fun_mix_EM_15Jan2018_marg_lik.R
+++ R/Fun_mix_EM_15Jan2018_marg_lik.R
@@ -11,6 +11,39 @@
 #'
 #' @return Vector of 3 elements. 1st, probability of p=0; 2nd, precision parameter of beta distribution,3rd: BF
 #' 
+#' @examples 
+#' 
+#' nsites = 500
+#'ncase = 200
+#'ncontrol = 200
+#'maf = 0.005
+#'rexp_param = 2
+#'
+#'model1 = function(i) {
+#'  
+#'  p1 = rbinom( ncontrol  , nsites , maf   )
+#'  p2 = rbinom( ncase     , nsites , maf )  + H1 * round(rexp(ncase,rexp_param))
+  
+#'  disease_status = c (  rep(0,ncontrol),rep(1,ncase)  )
+#'  data = data.frame(c(p1,p2), disease_status)
+  
+#'  bf = BFmixture(data, nsites)[3]
+#'  bf
+#'}
+#'
+#'nrep = 20
+#'
+#'H1 = 0
+#'bf1 = ( replicate(nrep,model1() ) )
+#'
+#'H1 = 1
+#'bf2 = ( replicate(nrep,model1() ) )
+#'
+#'col = c( rep("blue",nrep) , rep("red", nrep))
+#'
+#'
+#'plot(log(c(bf1,bf2)),col = col ,pch=20,cex=1.5)
+#' 
 #' @export
 #' 
 BFmixture = function(obs.data, nvariants, low.bound = 10^(-2) ) {
